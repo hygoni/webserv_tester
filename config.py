eaba0be305d945b2335083347466a12a3b520b35
유형곤 <hyeyoo@hygoni.local>
2020-11-05 06:36:02 +0900

    [ADD] add request_line.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 SERVER_ADDR = '127.0.0.1'
 SERVER_PORT = 80
 
+# if server receive uri longer than max uri length, it should reponse with 414
+MAX_URI_LENGTH = 100000000
---
+++ testcase/request_line.py
@@ -0,0 +1,38 @@
+import sys
+sys.path.append('../')
+import config
+import socket
+from http.client import HTTPResponse
+
+# use raw socket to modify http version
+def run():
+    request_headers = []
+    expected_status = []
+
+    # multiple spaces
+    request_headers.append('GET  /  HTTP/1.1\r\nHost:{}\r\n\r\n'.format(config.SERVER_ADDR))
+    expected_status.append(400)
+
+    # too long URI
+    target = '/' + 'A' * (config.MAX_URI_LENGTH - 1)
+    request_headers.append('GET {} HTTP/1.1\r\nHost:{}\r\n\r\n'.format(target, config.SERVER_ADDR))
+    expected_status.append(414)
+
+    # run cases
+    for i in range(len(request_headers)):
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((config.SERVER_ADDR, config.SERVER_PORT))
+        request_header = request_headers[i]
+        status = expected_status[i]
+        client.send(request_header.encode())
+        # read and parse http response
+        http_response = HTTPResponse(client)
+        http_response.begin()
+        # 505 error is expected for invalid http version
+        if http_response.status != status:
+            print('error: {}'.format(__file__))
+            print('expected status: {}, actual status: {}'.format(str(status), str(http_response.status)))
+        client.close()
+
+if __name__ == '__main__':
+    run()
